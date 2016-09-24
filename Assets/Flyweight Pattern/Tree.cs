cbbd17e24173bc8bc8e5da5b073f7528f44cb149
Joseph Marritt <JosephRMarritt@gmail.com>
2016-09-24 21:47:07 +0100

    Started adding the basics from the Game Patterns book for the FlyWeight pattern

---
+++ Assets/Flyweight Pattern/Tree.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree
+{
+    TreeModel _model;
+
+    Vector3 _position;
+    float _height;
+    float _thickness;
+    Color _barkTint;
+    Color _leafTint;
+}
---
+++ Assets/Flyweight Pattern/TreeModel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeModel
+{
+    Mesh _mesh;
+    Texture _bark;
+    Texture _leaves;
+}
