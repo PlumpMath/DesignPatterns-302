eb8c064f4df4f3bb5ff9b96eb89f5ab8f33644f9
Joseph Marritt <JosephRMarritt@gmail.com>
2017-02-13 22:32:05 +0000

    Worked on the first steps to creating an observer pattern

---
+++ Assets/Observer Pattern/Achievements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievement
+{
+    public string AchievementName { get; set; }
+
+    public void Unlock()
+    {
+        GenericUIManager.ChangeText("You have unlocked the" + AchievementName + " achievement!!");
+    }
+
+}
+
+public class Achievements : MonoBehaviour, IObserver
+{
+    public Dictionary<String, Achievement> achievements = new Dictionary<string, Achievement>();
+
+    public void OnNotify(Entity entity, EventType eventToTrigger)
+    {
+        switch (eventToTrigger)
+        {
+            case EventType.PLAYER_GETS_50_BEANS:
+                if(entity == Entity.PLAYER && Player.beans >= 50)
+                {
+                    Unlock(achievements[eventToTrigger.ToString()]);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Unlock(Achievement achievement)
+    {
+        achievement.Unlock();
+    }
+
+}
---
+++ Assets/Observer Pattern/IObserver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventType
+{
+    PLAYER_GETS_50_BEANS,
+    PLAYER_SELLS_999_BEANS
+}
+
+public enum Entity
+{
+    PLAYER,
+    ENEMY,
+    BEAN_SALESMAN
+}
+
+public class Player
+{
+    public static int beans { get; set; }
+
+    Player()
+    {
+        beans = 50;
+    }
+}
+
+public interface IObserver 
+{
+    void OnNotify(Entity entity, EventType eventToTrigger);
+}
